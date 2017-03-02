91987741d9007e28e089a0718aa05d7716bd2386
Nitheesh <nitheeshas91@gmail.com>
2017-03-02 13:10:44 +0530

    Fix major bug in setup script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@ set -e
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
-
 ln .vimrc ~/.vimrc
 vim +PluginInstall +qall
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
 
 echo "Installation finished!"

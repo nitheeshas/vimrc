910c85f360208b2b5399a952303b5c09bec0cd5c
Nitheesh <nitheeshas91@gmail.com>
2017-10-26 23:12:51 +0530

    Fix minor error with setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln .vimrc ~/.vimrc
 vim +PluginInstall +qall
 
-ln .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/
+ln .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
 
